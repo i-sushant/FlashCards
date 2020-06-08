3d1f63ef228ec3745b784d76edae63ebb4b3031d
Sushant Gupta <imsushant25@gmail.com>
2020-06-08 22:40:40 +0530

    Stage 5 complete

---
+++ Flashcards/task/src/flashcards/Card.java
@@ -0,0 +1,10 @@
+package flashcards;
+
+public class Card {
+    String definition;
+    String name;
+    Card(String name, String definition) {
+        this.name = name;
+        this.definition = definition;
+    }
+}
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -45,20 +45,20 @@ public class Main {
     }
     public static void addCard(BufferedReader br)throws IOException {
             System.out.println("The card:");
-            String cardName =  br.readLine();
+            String cardName =  br.readLine().trim();
             if(map.containsKey(cardName)) {
                 System.out.println("The card \"" + cardName + "\" already exists. ");
                 return;
             }
             System.out.println("The definition of the card");
-            String cardDefinition = br.readLine();
+            String cardDefinition = br.readLine().trim();
             if(map2.containsKey(cardDefinition)) {
                 System.out.println("The definition \"" + cardDefinition + "\" already exists.");
                 return;
             }
             map.put(cardName,cardDefinition);
             map2.put(cardDefinition,cardName);
-            System.out.println("The pair (\""+cardName+"\":\""+cardDefinition+"\") has been added.");
+            System.out.println("The pair (\""+cardName+"\":\""+map.get(cardName)+"\") has been added.");
     }
     public static void removeCard(BufferedReader br)throws IOException {
         System.out.println("The card");
@@ -75,7 +75,7 @@ public class Main {
         System.out.println("File name:");
         String fileName = br.readLine();
         File file = new File(fileName);
-        if(!file.isFile()){
+        if(!file.isFile()) {
             System.out.println("File not found.");
             return;
         }
@@ -85,6 +85,10 @@ public class Main {
             String[] tokens = sc.nextLine().split(",");
             String key = tokens[0].trim();
             String value = tokens[1].trim();
+            if(map.containsKey(key)) {
+                String oldDefinition = map.get(key);
+                map2.remove(oldDefinition);
+            }
             map.put(key,value);
             map2.put(value,key);
             count++;
@@ -104,7 +108,7 @@ public class Main {
             String value = entry.getValue();
             sb.append(key).append(",").append(value).append("\n");
         }
-        writer.write(sb.toString());
+        writer.write(sb.toString().trim());
         writer.close();
         System.out.println(count + " cards have been saved.");
     }
@@ -114,15 +118,15 @@ public class Main {
         Random random = new Random();
         ArrayList<String> keys = new ArrayList<>(map.keySet());
         int limit = keys.size();
-        for(int i = 0; i<times;i++) {
+        for(int i = 0; i < times; i++) {
             String card = keys.get(random.nextInt(limit));
             System.out.println("Print the definition of \"" + card +"\":");
-            String query = br.readLine().trim();
+            String query = br.readLine();
             String correctAnswer = map.get(card);
             if(query.equals(correctAnswer)) {
                 System.out.println("Correct answer.");
             } else {
-                if(map2.containsKey(query)){
+                if(map2.containsKey(query)) {
                     System.out.println("Wrong answer. The correct one is \"" + correctAnswer + "\", you've just written the definition of \"" + map2.get(query) + "\".");
                 } else {
                     System.out.println("Wrong answer. The correct one is \"" + correctAnswer + "\".");
